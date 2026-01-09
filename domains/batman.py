6c25c33784cf7e4bd36a347010f301864c4e91cb
flohms18 <trouilletflorent@outlook.com>
2026-01-09 14:53:02 +0100

    Created Domains

---
+++ domains/batman.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Batman Domain")
+
+Batman_Data = [
+    {"firstname" : 'Bruce', 'lastname' : 'Wayne'}
+]
---
+++ domains/superman.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Superman Domain")
+
+Superman_Data = [
+    {"firstname" : 'Clarck', 'lastname' : 'Kent'}
+]
